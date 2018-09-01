8e48070170fb15de4e3afee9010313f45bcd22b0
Leandro A. Scholz <leandro.a.scholz@gmail.com>
2018-09-01 19:38:50 -0300

    added loop to extract node positions
    created a for loop that gets each position of the x,y and z coordinates
    and stores into the swcArray matrix.

--- learnET.py
+++ learnET.py
@@ -9,11 +9,14 @@
 # https://stackabuse.com/reading-and-writing-xml-files-in-python/
 # Leandro Scholz 29.08.2018
 
+#details about the swc format 
+# http://www.neuronland.org/NLMorphologyConverter/MorphologyFormats/SWC/Spec.html
 import xml.etree.ElementTree as ET
 import numpy as np
 
 #read the file
-tree=ET.parse('tree_structure.gxl')
+tree=ET.parse('D:\\Leandro\\MEGA\\MestradoPPGEQ\\NEUBIAS\\image2\\tree_structure.gxl')
+#tree=ET.parse('tree_structure.gxl')
 root=tree.getroot()
 
 # find all elements with name 'node' and 'edge'
@@ -30,6 +33,8 @@ print('there are ',num_nodes,' nodes in this graph')
 print('there are ',num_edges,' edges in this graph')
 
 swcArray=np.zeros((num_nodes,7))
+# adds to the first column the node id's 
+swcArray[:,0]=np.arange(num_nodes)
 print(swcArray)
 
 #testing what attributes are inside the nodes in this graph
@@ -52,21 +57,34 @@ for node in tree.findall('.//node/[@id]'):
 	node_id=node.get('id')
 	print(node_id)
 	node_count+=1
+	
 
 # test to check how it is possible to get the values of the 
 # node positions
 print('the position of the nodes are ')
-#print all node id's of the graph
+# print all node id's of the graph 
+# THIS PART IS STILL NOT WORKING PROPERLY
 for nodes in tree.findall(".//node/attr/[@name=' position']"):
-	for node in nodes:
-		print('node:')
-		for position in node:
-			node_pos=float(nodes.find('.//tup/float').text)
-			print(node_pos)			
+	positions=nodes.findall('.//tup/float')
+	for position in positions:
+		node_pos=float(position.text)
+		print(node_pos)
+			
+# With this, it's possible to visualize the positions of all nodes of the gxl file in the prompt
+# what I want is to store each os these positions to the correct row in swcArray. 
+# if node 2 position is [5,5,5] I want the second row of swcArray columns 3,4 and 5 (see .swc file format specification) to store this position
+for node_id in range(0,num_nodes):
+	xpath=".//node/[@id='n%s']/attr/[@name=' position']" %node_id
+	for node in tree.findall(xpath):
+		node_pos=node.findall('.//tup/float')
+		for position in range(0,3):
+			swcArray[node_id,position+2]=float(node_pos[position].text)
 
+# now the positions are stored in swcArray properly 
+print(swcArray)
 # now a similar test to check how to get the radius of the edge
 # for later, I will probably define the radius of a nodes as being the average radius of its edges
-print('the radius of the edges are')
+print('the radii of the edges are')
 for edges in tree.findall(".//edge/attr/[@name=' radius']"):
 	for edge in nodes:
 		edge_rad=float(edges.find('.//float').text)
